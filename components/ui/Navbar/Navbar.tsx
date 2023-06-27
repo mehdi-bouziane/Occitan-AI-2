c379eeb78709ec026b762eb245f181a5030b19be
mehdi-bouziane <bouziane-mehdi@hotmail.fr>
2023-06-27 23:10:26 +0200

    fix navbar hidden mobile

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -28,11 +28,11 @@ export default async function Navbar() {
       </a>
       <div className="max-w-6xl px-6 mx-auto">
         <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
-          <div className="flex items-center flex-1">
+          <div className="flex items-center flex-2">
             <Link href="/" className={s.logo} aria-label="Logo">
               <Logo />
             </Link>
-            <nav className="hidden ml-6 space-x-2 lg:block">
+            <nav className="ml-6 space-x-2 lg:block">
               {user && subscription != null && (
                 <Chat />
               )}
